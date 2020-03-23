8460d91f0ca7b9915f79add3d48e1a71ce2bf540
Andriy Kyrylenko <z9877969@ukr.net>
2020-03-23 23:36:18 +0200

    Updates

---
+++ precache-manifest.4a9b9c69e526f917adaa9301a72eef3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fa6e9704043306338eff518eb6e573",
+    "url": "/calendar-filter/index.html"
+  },
+  {
+    "revision": "47fd56190ffdcd36bb45",
+    "url": "/calendar-filter/static/css/main.52b7f1d5.chunk.css"
+  },
+  {
+    "revision": "103fba4627ef9f9ab66c",
+    "url": "/calendar-filter/static/js/2.5efee774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calendar-filter/static/js/2.5efee774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47fd56190ffdcd36bb45",
+    "url": "/calendar-filter/static/js/main.5ca4460e.chunk.js"
+  },
+  {
+    "revision": "48c5c0013df7e7194db6",
+    "url": "/calendar-filter/static/js/runtime-main.a93d2af0.js"
+  }
+]);
